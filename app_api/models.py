a561e475c8bb15313e46894d028759e99839e7fa
UrinbaevZulfiqor1201 <zulfiqor1201@gmail.com>
2024-11-16 18:09:33 +0500

    the first commit

---
+++ app_api/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'username']
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
